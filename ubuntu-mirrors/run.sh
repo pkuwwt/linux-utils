891d78356ed76474e02b4ddda8b5d029655d927e
ubuntu <ubuntu@127.0.0.1>
2019-08-01 00:46:21 +0800

    init

---
+++ ubuntu-mirrors/run.sh
@@ -0,0 +1,17 @@
+
+# cat docker-ce-gpg | sudo apt-key add -
+wget https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu/gpg -O docker-ce-gpg
+wget https://mirrors.tuna.tsinghua.edu.cn/help/docker-ce/ -O docker-ce.html
+
+# cat postgresql-ACCC4CF8.asc | sudo apt-key add -
+wget https://mirrors.tuna.tsinghua.edu.cn/postgresql/repos/apt/README -O postgresql-README
+wget https://mirrors.tuna.tsinghua.edu.cn/postgresql/repos/apt/ACCC4CF8.asc -O postgresql-ACCC4CF8.asc
+
+# cat 314DF160.gpg | sudo apt-key add -
+gpg --export --armor 314DF160
+gpg --export --armor 314DF160 >314DF160.gpg
+
+BASE_PATH=`dirname $(readlink -f $0)`
+cat $BASE_PATH/mirror-template.list | sed "s,BASE_PATH,$BASE_PATH,g" >$BASE_PATH/mirror.list
+apt-mirror $BASE_PATH/mirror.list
+
